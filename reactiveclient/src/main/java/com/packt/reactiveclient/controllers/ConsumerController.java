cce2c83948e2e943855efcb54045278c0767173a
Bram Eldering <bram.eldering@gmail.com>
2025-10-16 12:47:23 +0200

    Added logging

--- reactiveclient/src/main/java/com/packt/reactiveclient/controllers/ConsumerController.java
+++ reactiveclient/src/main/java/com/packt/reactiveclient/controllers/ConsumerController.java
@@ -1,6 +1,8 @@
 package com.packt.reactiveclient.controllers;
 
 import com.packt.reactiveclient.records.Card;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,22 +18,27 @@ import reactor.core.publisher.Mono;
 public class ConsumerController {
   private final WebClient webClient;
 
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
   public ConsumerController(@Value("${footballservice.url}") String url) {
+    logger.info("ConsumerController initialized");
     this.webClient = WebClient.create(url);
   }
 
   @GetMapping("/cards")
   public Flux<Card> getCards() {
-    return webClient.get().uri("/cards").retrieve().bodyToFlux(Card.class);
+    return webClient.get().uri("/cards").retrieve().bodyToFlux(Card.class).doOnSubscribe(sub -> logger.info("Received call to getCards"));
   }
 
   @GetMapping("/cards/{cardId}")
   public Mono<Card> getCard(@PathVariable("cardId") String cardId) {
-    return webClient.get().uri("/cards/" + cardId).retrieve().onStatus(code -> code.is4xxClientError(), response -> Mono.empty()).bodyToMono(Card.class);
+    logger.info("getCard {}", cardId);
+    return webClient.get().uri("/cards/" + cardId).retrieve().onStatus(code -> code.is4xxClientError(), response -> Mono.empty()).bodyToMono(Card.class).doOnSubscribe(sub -> logger.info("Received call to getCard")).doOnNext(card -> logger.info("Received card: {}", card)).doOnError(error -> logger.error("Error fetching card {}", cardId, error));
   }
 
   @GetMapping("/error")
   public Mono<String> getFailedRequest() {
+    logger.info("getFailedRequest");
     return webClient.get().uri("/invalidpath").exchangeToMono(response ->{
       if (response.statusCode().equals(HttpStatus.NOT_FOUND)) return Mono.just("Remote server returned 404");
       else if (response.statusCode().equals(HttpStatus.INTERNAL_SERVER_ERROR)) return Mono.just("Remote server returned 500: " + response.bodyToMono(String.class));
